69f1c518eb3d5cb24eb6613c948912d7bfb01a36
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-06-12 17:17:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function distanceTravelledInFeet(begin, end) {
 
 function calculatesFarePrice(begin, end) {
   const distance = distanceTravelledInFeet(begin, end);
-  if distance <= 400 {
+  if (distance <= 400) {
     return 0;
   } else if (distance > 400 && distance <= 2000) {
     return distance * .02;
